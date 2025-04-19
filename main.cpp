048b98187fc956a70ec4813e7263b89f13693d73
Robert Crummett <rncrummett5@gmail.com>
2025-04-18 19:36:03 -0700

    Working on forward. Not tested yet.

--- main.cpp
+++ main.cpp
@@ -55,18 +55,33 @@ static void PrintMatrix(const double* Matrix, int Rows, int Cols) {
         std::cout << std::endl;
 }
 
+static double* Forward(DenseLayer* Layer, double* Matrix, int BatchNumber, double* Output) {
+        double Alpha = 1.0;
+        double Beta = 0.0;
+
+        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 
+                Layer->InputNumber, BatchNumber, Layer->NeuronNumber,
+                Alpha, Layer->Weights, Layer->NeuronNumber,
+                Matrix, BatchNumber, Beta, Output, BatchNumber);
+}
+
 int main() {
-        int InputNumber = 4;
-        int NeuronNumber = 3;
+        int InputNumber = 2;
+        int NeuronNumber = 8;
+        int OutputNumber = 2;
 
-        DenseLayer* Layer = AllocateDenseLayer(InputNumber, NeuronNumber);
+        DenseLayer* Layer1 = AllocateDenseLayer(InputNumber, NeuronNumber);
+        DenseLayer* Layer2 = AllocateDenseLayer(NeuronNumber, NeuronNumber);
+        DenseLayer* Layer3 = AllocateDenseLayer(NeuronNumber, OutputNumber);
 
-        std::cout << "Weights:" << std::endl;
-        PrintMatrix(Layer->Weights, InputNumber, NeuronNumber);
-        std::cout << "Biases:" << std::endl;
-        PrintMatrix(Layer->Biases, 1, NeuronNumber);
+        std::cout << "Weights (Layer1):" << std::endl;
+        PrintMatrix(Layer1->Weights, Layer1->InputNumber, Layer1->NeuronNumber);
+        std::cout << "Biases (Layer1):" << std::endl;
+        PrintMatrix(Layer1->Biases, 1, Layer1->NeuronNumber);
 
-        FreeDenseLayer(Layer);
+        FreeDenseLayer(Layer1);
+        FreeDenseLayer(Layer2);
+        FreeDenseLayer(Layer3);
         
         return 0;
 }
